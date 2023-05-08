2b941460596da358bd1b469295e42bc05da876ea
Kleber <dev.nota10.k@gmail.com>
2023-05-08 11:26:10 -0300

    added box-florest and containe- wall

--- script/script.js
+++ script/script.js
@@ -8,4 +8,36 @@ btnMobile = document.querySelector("#btn-mobile").addEventListener('click',()=>{
 })
 
 
+// Animation motorcycle
+animeMotor()
+ function animeMotor(){
+    const moto = document.querySelector(".motorcycle-wall").addEventListener('click',()=>{
+        const moveMoto = document.querySelector('.motorcycle')
+        
+        setTimeout(function(){
+            moveMoto.style.animation =" move-motorcycle ease  15s infinite";
+            const sound = document.querySelector('#sound').play()
+        },2000)
+    })
+ }
+
+//  Description of car:
+    const imgCar = document.querySelectorAll(".img-car");
+    imgCar.forEach((el,i)=>{
+        el.addEventListener('click',()=>{
+           console.log(el)
+            
+        })
+    })
+
+
+    // conf click car 
+    // function textCar(){
+    //     const text =  document.querySelectorAll(".text-car")
+    //     const amtText = text;
+        
+
+    // }
+
+ 
 }
